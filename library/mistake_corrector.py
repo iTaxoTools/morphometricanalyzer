4b84fd5d417bafaf92fed313c68b564c453bd9cd
Vladimir Kharchev <necrosovereign@gmail.com>
2020-09-04 18:33:26 +0200

    Mistake_Corrector class

--- library/mistake_corrector.py
+++ library/mistake_corrector.py
@@ -1,5 +1,5 @@
 from record import *
-from typing import Set, List, Tuple, TextIO, Optional
+from typing import Set, List, Tuple, TextIO, Optional, Iterator
 
 
 class HeaderFixer():
@@ -13,13 +13,20 @@ class HeaderFixer():
         """
         Initialize with the header line of the data file.
         """
-        self.fields: List[Optional[str]] = [field.strip()
+        self.fields: List[Optional[str]] = [field.strip().casefold()
                                             for field in header.split('\t')]
         self.field_names_corrections: List[Tuple[str, str]] = []
         self.metafields_ordered = True
         self.bad_metafields: List[str] = []
         self.nonessential_missing = False
         self.essential_missing = False
+        self.non_unique_variables: List[str] = []
+        self.correct_metafields()
+        self.verify_metafields()
+        self.check_missing_metafields()
+        self.make_unique_variable_names()
+        self.variables = set(self.fields) - set(HeaderFixer.required_fields)
+        self.variables.remove(None)
 
     def correct_metafield(self, correct_name: str, variants: Set[str]) -> None:
         """
@@ -59,6 +66,7 @@ class HeaderFixer():
         self.metafields_ordered = sorted(metafield_perm) == metafield_perm
         # calculate the length of the metafield portion of the header
         metafield_len = max(metafield_perm) + 1
+        self.variables_num = len(self.fields) - metafield_len
         # collect the extraneous fields and delete them
         for i, field in enumerate(self.fields[0:metafield_len]):
             if field not in set(HeaderFixer.required_fields):
@@ -79,6 +87,18 @@ class HeaderFixer():
             self.essential_missing = True
         return False
 
+    def make_unique_variable_names(self) -> None:
+        """
+        Append running numbers to the repeated variable names.
+        """
+        for i, var in enumerate(self.fields):
+            if var is None:
+                continue
+            repeated = self.fields[0:i].count(var)
+            if repeated:
+                self.fields[i] = var + str(repeated)
+                self.non_unique_variables.append(var)
+
     def report(self, output_file: TextIO) -> None:
         """
         Write the output of the correction into the output_file
@@ -90,14 +110,132 @@ class HeaderFixer():
                                corrected_name in self.field_names_corrections])),
                   file=output_file)
         if not self.metafields_ordered:
-            print("Metadata columns are expected in the order specimenid, species, sex, locality. In the input file the order of these columns appears to be different. The program will proceed with the analyses, but please check if there may be a confusion affecting the metadata.")
+            print("Metadata columns are expected in the order specimenid, species, sex, locality. In the input file the order of these columns appears to be different. The program will proceed with the analyses, but please check if there may be a confusion affecting the metadata.", sep='', file=output_file)
         if self.essential_missing:
-            print("The input file lacks at least one of the two columns, specimenid and species, that are required to run the analyses, and the program therefore could not be executed.")
+            print("The input file lacks at least one of the two columns, specimenid and species, that are required to run the analyses, and the program therefore could not be executed.", sep='', file=output_file)
         if self.nonessential_missing:
-            print("The input file lacks at least one of the two columns, locality and sex, and certain analyses can therefore not be performed.")
+            print("The input file lacks at least one of the two columns, locality and sex, and certain analyses can therefore not be performed.", sep='', file=output_file)
         if self.bad_metafields:
             print("The following column(s) have been identified interspersed among the metedata columns: ",
                   ", ".join(self.bad_metafields),
-                  ". These columns will not be considered to contain variables and will be ignored in all further analyses. If this is not intended, please change order of columns and repeat analysis.")
+                  ". These columns will not be considered to contain variables and will be ignored in all further analyses. If this is not intended, please change order of columns and repeat analysis.", sep='', file=output_file)
+        if self.non_unique_variables:
+            print("The following non-unique variable names were detected:",
+                  ", ".join(self.non_unique_variables),
+                  ". These have been renamed by appending numbers.", sep='', file=output_file)
 
-# class MistakeCorrector():
+
+class MistakeCorrector():
+    """
+    Iterates over the lines of a tab-separated data file and corrects certain mistakes.
+
+    Yields corrected lines.
+    """
+
+    def __init__(self, lines: Iterator[str]):
+        try:
+            header = next(lines)
+        except StopIteration:
+            raise ValueError("The input file is empty")
+        self.lines = lines
+        self.header_fixer = HeaderFixer(header)
+        self.at_header = True
+        self.unique_specimenids: Set[str] = set()
+        self.non_unique_specimenids: Set[str] = set()
+        self.unusual_sexes: Set[str] = set()
+        self.numbers_with_commas = False
+        self.invalid_content_count = 0
+        self.vars_with_numbers: Set[str] = set()
+
+    def report(self, output_file: TextIO) -> None:
+        self.header_fixer.report(output_file)
+        if self.non_unique_specimenids:
+            print("The column specimenid contains the following non-unique values which may indicate that measurements of one or several specimens will be entered twice in the analysis: ",
+                  ", ".join(self.non_unique_specimenids), ".", sep='', file=output_file)
+        if self.unusual_sexes:
+            print("The column sex typically accepts as values only male, female, juvenile and larva, but it was found to also contain the following ones: ",
+                  ", ".join(self.unusual_sexes), ".", sep='', file=output_file)
+        if self.numbers_with_commas:
+            print("Some or all variable fields contained values with commas; these have been converted to periods assuming that they were meant to be decimal separators as usual e.g. in Spanish, French or German.",
+                  file=output_file)
+        if self.invalid_content_count:
+            print("A total of {self.invalid_content_count} cases with invalid content (not a number) were detected in the fields of one or several variables; this content has been deleted and in the analysis is treated as missing value.", file=output_file)
+        empty_columns = self.header_fixer.variables - self.vars_with_numbers
+        if empty_columns:
+            for var in empty_columns:
+                print(
+                    "Variable {var} does not appear to contain numerical values and is therefore excluded from all calculations.", file=output_file)
+
+    def __iter__(self) -> "MistakeCorrector":
+        return self
+
+    def verify_unique_specimenid(self, record: Record) -> None:
+        """
+        Check that the record has a unique specimenid
+
+        Writes errors into the Remark field
+        """
+        if record.specimenid in self.unique_specimenids:
+            self.non_unique_specimenids.add(record.specimenid)
+            record.remark_add("Non-unique specimenid value detected.")
+        else:
+            self.unique_specimenids.add(record.specimenid)
+
+    def verify_sex(self, record: Record) -> None:
+        """
+        Corrects the sex attribute of the Record
+
+        variants of male, female, juvenile and larva are corrected
+
+        other values are noted
+        """
+        corrections = dict(
+            male={"m", "m.", "mâle", "macho",
+                  "Maennchen", "Männchen", "männlich"},
+            female={"f", "f.", "fem", "fem.", "femelle", "hembra", "Weibchen"},
+            juvenile={"j", "j.", "juv", "juv.", "juvenil"},
+            larva={"l", "l.", "larv", "larv.", "larvae", "larve"}
+        )
+        sex = record.sex.casefold()
+        if sex not in corrections:
+            self.unusual_sexes.add(sex)
+            record.remark_add(
+                "The column sex contains a category different from male, female, juvenile or larva")
+            return
+        for standard_sex, variants in corrections.items():
+            if sex in variants:
+                record.sex = standard_sex
+                break
+
+    def correct_values(self, record: Record) -> None:
+        """
+        Corrects the numerical values of variables
+
+        Deletes non-numerical values
+        """
+        for field, value in record.variables.items():
+            if ',' in value:
+                value = value.replace(',', '.')
+                self.numbers_with_commas = True
+            try:
+                value = str(float(value))
+            except ValueError:
+                value = ""
+                self.invalid_content_count += 1
+            else:
+                self.vars_with_numbers.add(field)
+            record.variables[field] = value
+
+    def __next__(self) -> str:
+        if self.at_header:
+            self.at_header = False
+            return self.yield_header()
+        row = next(self.lines)
+        record = Record(self.header_fixer.fields, row)
+        self.verify_unique_specimenid(record)
+        self.verify_sex(record)
+        self.correct_values(record)
+        return record.to_row()
+
+    def yield_header(self) -> str:
+        return "\t".join(field if field is not None else "" for field in self.header_fixer.fields) + "\t" + "Remark"
--- library/record.py
+++ library/record.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 
 class Record():
@@ -11,18 +11,26 @@ class Record():
     variables attribute contains the variables in the row, excluding the metadata
     """
 
-    metadata_fields = {'specimenid', 'species', 'locality', 'sex'}
+    metadata_fields = {'specimenid', 'species', 'sex', 'locality'}
 
-    def __init__(self, fields: List[str], row: str):
+    def __init__(self, fields: List[Optional[str]], row: str):
         """
         Initializes a record with fields from fields and value from row.
         row is a line of tab-separated data file.
         """
         # subdictionary that contains only the variables
+        self.fields = fields
         self.variables: Dict[str, str] = {}
+        self.specimenid = ""
+        self.species = ""
+        self.sex = ""
+        self.locality = ""
+        self.remark = ""
         for field, value in zip(fields, row.split('\t')):
             value = value.strip()  # remove the surrounding spaces
-            if field in Record.metadata_fields:
+            if field is None:
+                continue
+            elif field in Record.metadata_fields:
                 # metadata fields
                 self.__setattr__(field, value)
             else:
@@ -38,8 +46,35 @@ class Record():
         else:
             return self.variables[key]
 
+    def get(self, key: str, default: Optional[str] = None) -> Optional[str]:
+        """
+        Same as dict.get
+        """
+        if key in Record.metadata_fields:
+            try:
+                return self.__getattribute__(key)
+            except AttributeError as ex:
+                return default
+        else:
+            return self.variables.get(key, default)
+
     def __setitem__(self, key: str, value: str) -> None:
         if key in Record.metadata_fields:
             self.__setattr__(key, value)
         else:
             self.variables[key] = value
+
+    def to_row(self) -> str:
+        """
+        Prints the record as a row of a tab-separated data file
+        """
+        return "\t".join(self[field] if field is not None else "" for field in self.fields) + f"\t{self.remark}"
+
+    def remark_add(self, remark: str) -> None:
+        """
+        Adds remark into self.remark, adding semicolon if required
+        """
+        if self.remark:
+            self.remark += f"; {remark}"
+        else:
+            self.remark = remark
