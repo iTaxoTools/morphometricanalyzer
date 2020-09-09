5a841cd4437f22cf22a8662195587d08870f93cd
Vladimir Kharchev <necrosovereign@gmail.com>
2020-09-09 18:54:51 +0200

    Added support for multiple analyses with different groupings

--- library/analyses.py
+++ library/analyses.py
@@ -1,4 +1,4 @@
-from typing import TextIO, Set
+from typing import TextIO, Set, List
 import pandas as pd
 
 
@@ -10,12 +10,28 @@ def mean_and_others(col: pd.Series) -> str:
         return f"{col.mean():.3f} ± {col.std():.3f} ({col.min():.3f} - {col.max():.3f}), N = {num}"
 
 
-def analyse(buf: TextIO, output_file: TextIO, variables: Set[str]) -> None:
+def analyse(buf: TextIO, output_file: TextIO, variables: Set[str], analyses: List[List[str]]) -> None:
     table = pd.read_table(buf, usecols=(
         ['specimenid', 'species', 'sex', 'locality'] + list(variables)))
+    for analysis in analyses:
+        do_analysis(table, variables, analysis, output_file)
+        output_file.write("\n")
+
+
+def do_analysis(table: pd.DataFrame, variables: Set[str], analysis: List[str], output_file: TextIO) -> None:
+    # create new column names
     meanvar_rename = {var: f"Mean{var.capitalize()}" for var in variables}
-    meantable = table.groupby('species').mean().rename(columns=meanvar_rename)
+
+    # groupby doesn't behave as needed if analysis is empty
+    groupedtable = table.groupby(analysis) if analysis else table
+
+    # table of means
+    meantable = groupedtable.mean().rename(columns=meanvar_rename)
     meantable.to_csv(output_file, float_format="%.3f", sep='\t')
-    mean_and_others_table = table.groupby('species').aggregate(
+
+    output_file.write("\n")
+
+    # table of means, stds, mins, maxes and counts
+    mean_and_others_table = groupedtable.aggregate(
         mean_and_others).rename(columns=meanvar_rename)
     mean_and_others_table.to_csv(output_file, float_format="%.3f", sep='\t')
--- morphometricanalyzer.py
+++ morphometricanalyzer.py
@@ -22,6 +22,8 @@ def gui_main() -> None:
     table_chooser = FileChooser(
         mainframe, label="Modified table file", mode="save")
 
+    analyses_widget = AnalysesWidget(mainframe)
+
     def process_table() -> None:
         input_file = input_chooser.file_var.get()
         output_file = output_chooser.file_var.get()
@@ -36,7 +38,8 @@ def gui_main() -> None:
                     print(line, file=buf)
                 corrector.report(output_file)
                 buf.seek(0, 0)
-                analyse(buf, output_file, corrector.header_fixer.variables)
+                analyse(buf, output_file,
+                        corrector.header_fixer.variables, analyses_widget.get())
         except ValueError as ex:
             tk.messagebox.showerror("Error", str(ex))
         except FileNotFoundError as ex:
@@ -48,10 +51,35 @@ def gui_main() -> None:
 
     process_btn = ttk.Button(mainframe, text="Process", command=process_table)
 
+    set_num_analyses_frm = ttk.Frame(mainframe)
+    num_analyses_lbl = ttk.Label(
+        set_num_analyses_frm, text="Number of analyses")
+    num_analyses_var = tk.StringVar()
+    num_analyses_entr = ttk.Entry(
+        set_num_analyses_frm, textvariable=num_analyses_var)
+
+    def set_num_analyses() -> None:
+        try:
+            num = int(num_analyses_var.get())
+        except ValueError:
+            return
+        else:
+            analyses_widget.set_count(num)
+
+    num_analyses_btn = ttk.Button(
+        set_num_analyses_frm, text="Set", command=set_num_analyses)
+
     input_chooser.grid(row=0, column=0, sticky="nsew")
     output_chooser.grid(row=0, column=1, sticky="nsew")
     table_chooser.grid(row=0, column=2, sticky="nsew")
     process_btn.grid(row=1, column=1)
+
+    num_analyses_lbl.grid(row=0, column=0)
+    num_analyses_entr.grid(row=0, column=1)
+    num_analyses_btn.grid(row=0, column=2)
+    set_num_analyses_frm.grid(row=2, column=0, sticky='w')
+
+    analyses_widget.grid(row=3, column=0, sticky='w')
 
     mainframe.grid(row=0, column=0, sticky="nsew")
 
@@ -67,7 +95,8 @@ def main() -> None:
             print(line)
         corrector.report(sys.stderr)
         buf.seek(0, 0)
-        analyse(buf, sys.stdout, corrector.header_fixer.variables)
+        analyse(buf, sys.stdout,
+                corrector.header_fixer.variables, [['species']])
     else:
         gui_main()
 
